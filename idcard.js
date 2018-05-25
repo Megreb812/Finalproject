9b0068a3d834f9e5f04ed8057b4d3f08372acbcc
Megreb812 <39529989+Megreb812@users.noreply.github.com>
2018-05-25 16:03:25 -0700

    friday push

---
+++ idcard.js
@@ -0,0 +1,15 @@
+
+function idcard() {
+    let Firstname = document.getElementById('FirstnameInput').value;
+    let Lastname = document.getElementById('LastnameInput').value;
+    document.getElementById('postFullname').innerHTML = Firstname + '' + Lastname;
+    document.getElementById('postAddress').innerHTML = Address;
+    document.getElementById('age').innerHTML = Age;
+    document.getElementById('phoneNumber').innerHTML = number;
+}
+
+var numberArray= [Age, number];  
+
+ 
+
+
