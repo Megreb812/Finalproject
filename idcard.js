4e392f5455a5de7486dc55ef29b2d672f114dcc8
Megreb812 <39529989+Megreb812@users.noreply.github.com>
2018-05-28 20:04:49 -0700

    lets push

--- idcard.js
+++ idcard.js
@@ -1,15 +1,29 @@
 
 function idcard() {
-    let Firstname = document.getElementById('FirstnameInput').value;
-    let Lastname = document.getElementById('LastnameInput').value;
-    document.getElementById('postFullname').innerHTML = Firstname + '' + Lastname;
-    document.getElementById('postAddress').innerHTML = Address;
-    document.getElementById('age').innerHTML = Age;
-    document.getElementById('phoneNumber').innerHTML = number;
+    let Firstname = document.getElementById('Firstname').value;
+    let Lastname = document.getElementById('Lastname').value;
+    document.getElementById('postFullname').innerHTML = Firstname + ' ' + Lastname;
+    
+    let postAddress = document.getElementById('Address').value;
+    document.getElementById('postAddress').innerHTML = postAddress;
+
+    let age = document.getElementById('Age').value;
+    document.getElementById('age').innerHTML = age;
+    
+    let phoneNumber = document.getElementById('number').value;
+    document.getElementById('phoneNumber').innerHTML = phoneNumber;
+
+let numberArray= [age, phoneNumber];  
+    numberArray.push(age);
+    numberArray.push(phoneNumber);
+
+    for (let i = 0;i < numberArray.length; i++) 
+    
+        if (numberArray[i] <=100)
+            document.getElementById('postAge').innerHTML = 'Age: '+age;
+        if (numberArray[i] >100)
+            document.getElementById('postPhoneNumber').innerHTML = 'Phone Number: '+phoneNumber;
+
 }
-
-var numberArray= [Age, number];  
-
- 
 
 
